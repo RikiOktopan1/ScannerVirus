f3a9263b2dae74de9756eb119c79705aaeb7aa85
RikiOktopan1 <32426243+RikiOktopan1@users.noreply.github.com>
2017-11-26 20:23:53 +0700

    Create Clean.sh

--- Clean.sh
+++ Clean.sh
@@ -16,7 +16,7 @@ echo "Selesai.."
 echo " "
 echo "Catatan :"
 echo "Apabila terdapat Virus atau Junk File maka akan otomatis di hapus"
--e "\033[1m \e[32m" =================================
+echo " =================================
  
 echo "Copyright (c) 2017 By RikiOktopan"
 echo "================================="
